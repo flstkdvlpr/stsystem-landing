f6d236106a727fbc36d98361f8d34ed6131d19d8
flstkdvlpr <sartvas.ne@yahoo.com>
2022-10-18 16:02:37 +0300

    реализация логики для плоских кровель. Закончил

--- js/bundle.js
+++ js/bundle.js
@@ -353,23 +353,36 @@ function setcards(){
         })
     }
 
-    const actionForCards = function (classTag){
+    const actionOnForCards = function (classTag){
         document.querySelector(classTag).classList.remove('hide');
         document.querySelector(classTag).classList.add('animate__animated', 'animate__zoomIn');
     }
 
+    const actionOffOnForCards = function (classTag){
+        document.querySelector(classTag).classList.add('hide');
+    }
+
     setCards.forEach(item => {
         item.addEventListener('click', (e)=>{
-            e.preventDefault();
             if(e.target && e.target.dataset.value == "proof"){
                 setCards.forEach(item =>{
                     item.classList.add('animate__animated', 'animate__zoomOut');
                     setTimeout(hideAllSets, 500);
-                    actionForCards('.set-item-proof');
+                    actionOnForCards('.set-item-proof');
+                    setTimeout(hideAllSets, 500);
                 })
 
-                document.querySelector('.set-tittle').innerHTML = "<a name='catalog'>Каталог</a>";
+                document.querySelector('.set-tittle').innerHTML = '<a href="http://stsystem-landing/#sets" name="sets" class="set-href">Вернуться к наборам</a>';
             }
+        })
+    })
+
+    document.querySelector('.set-tittle').addEventListener('click', ()=>{
+        setCards.forEach(item =>{
+            item.classList.remove('animate__animated', 'animate__zoomOut');
+            showAllSets();
+            actionOffOnForCards('.set-item-proof');
+            document.querySelector('.set-tittle').innerHTML = '<a name="sets">Наборы</a></h2>';
         })
     })
 
