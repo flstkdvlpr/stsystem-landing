5b1715070778be5f45b5038c6f0e92d63e862992
flstkdvlpr <sartvas.ne@yahoo.com>
2022-10-19 15:21:30 +0300

    Рефакторинг функций сокрытия списка готовых решений и появления подсписка.

--- js/modules/setcards.js
+++ js/modules/setcards.js
@@ -8,45 +8,55 @@ function setcards(){
             item.closest(".set-dachbords").style.display = 'none';
         })
     }
-
     function showAllSets (){
         setCards.forEach(item =>{
             item.closest(".set-dachbords").style.display = 'block';
         })
     }
-
     const actionOnForCards = function (classTag){
         document.querySelector(classTag).classList.remove('hide');
         document.querySelector(classTag).classList.add('animate__animated', 'animate__zoomIn');
     }
-
     const actionOffOnForCards = function (classTag){
         document.querySelector(classTag).classList.add('hide');
     }
 
-    setCards.forEach(item => {
-        item.addEventListener('click', (e)=>{
-            if(e.target && e.target.dataset.value == "proof"){
-                setCards.forEach(item =>{
-                    item.classList.add('animate__animated', 'animate__zoomOut');
-                    setTimeout(hideAllSets, 500);
-                    actionOnForCards('.set-item-proof');
-                    setTimeout(hideAllSets, 500);
-                })
-
-                document.querySelector('.set-tittle').innerHTML = '<a href="http://stsystem-landing/#sets" name="sets" class="set-href">Вернуться к наборам</a>';
-            }
+    function closeSetShowSubset(classOfSelect, valueOfHref){
+        setCards.forEach(item => {
+            item.addEventListener('click', (e)=>{
+                if(e.target && e.target.dataset.value == valueOfHref){
+                    setCards.forEach(item =>{
+                        item.classList.add('animate__animated', 'animate__zoomOut');
+                        setTimeout(hideAllSets, 500);
+                        actionOnForCards(classOfSelect);
+                        setTimeout(hideAllSets, 500);
+                    })
+                    document.querySelector('.set-tittle').innerHTML = '<a href="http://stsystem-landing/#sets" name="sets" class="set-href">Вернуться к наборам</a>';
+                }
+            })
         })
-    })
+    }
 
-    document.querySelector('.set-tittle').addEventListener('click', ()=>{
-        setCards.forEach(item =>{
-            item.classList.remove('animate__animated', 'animate__zoomOut');
-            showAllSets();
-            actionOffOnForCards('.set-item-proof');
-            document.querySelector('.set-tittle').innerHTML = '<a name="sets">Наборы</a></h2>';
+    function showSetcloseSubset(classOfSelect){
+        document.querySelector('.set-tittle').addEventListener('click', ()=>{
+            setCards.forEach(item =>{
+                item.classList.remove('animate__animated', 'animate__zoomOut');
+                showAllSets();
+                actionOffOnForCards(classOfSelect);
+                document.querySelector('.set-tittle').innerHTML = '<a name="sets">Наборы</a></h2>';
+            })
         })
-    })
+
+    }
+
+    closeSetShowSubset('.set-item-proof', "proof");
+    showSetcloseSubset('.set-item-proof');
+    closeSetShowSubset('.set-item-froof', "froof");
+    showSetcloseSubset('.set-item-froof');
+
+    
+
+    
 
 }
 
